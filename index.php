f4808348c91382cabbc36fd14da0cc5355308220
Afron Orana <afron@sprigs.eu>
2020-01-13 16:11:47 +0100

    Update bootstrap, change welcome screen

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php include('partials/header.php'); ?>
-    <test-component></test-component>
 
+<div class="container">
+
+    <h1>Welcome to the <a href="https://sprigs.eu/" target="_blank">SPRIGS_</a> Vue boilerplate!</h1>
+    <p>
+        To get started check out the docs <a href="#">here</a>!
+    </p>
+
+</div>
+
+<test-component></test-component>
 <?php include('partials/footer.php'); ?>
--- resources/assets/js/components/TestComponent.vue
+++ resources/assets/js/components/TestComponent.vue
@@ -1,6 +1,22 @@
 <template>
-    <div class="text-center">
-        <h3>Say hello to my little test component!</h3>
+    <div id="test-component">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 offset-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-image"></div>
+                            <img
+                                src="https://vuejs.org/images/logo.png"
+                                alt="vue-logo"
+                                class="text-center"
+                            />
+                            <h3 class="text-center">Say hello to my little vue component!</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -17,4 +33,21 @@ export default {
     methods: {}
 };
 </script>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+#test-component { 
+    animation: fadeIn 300ms 1s linear forwards;
+    opacity: 0;
+    padding: 5rem 0;
+}
+img {
+    display: block;
+    margin: 0 auto 20px;
+    width: 50px;
+}
+
+@keyframes fadeIn {
+    from { opacity: 0; }
+    to { opacity: 1; }
+}
+</style>
